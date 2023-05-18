ea363bdad88e448020cd6bfd4538c4cbc1c2d860
beketamas <tamas.beke0921@gmail.com>
2023-05-18 13:55:11 +0200

    kesz

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DolgozatWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szakasz> lista = new List<Szakasz>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            StreamReader str = new StreamReader("kektura.txt");
+            while (!str.EndOfStream) 
+            {
+                lista.Add(new Szakasz(str.ReadLine()));
+            }
+            dgTablauzat.ItemsSource = lista;
+
+            Szakasz.NehezsegMegadas(lista);
+            File.WriteAllLines("utvonal.txt", lista.Select(x => $"{x.Nev};{x.Tavolsag};{x.TengerszintFeletti};{x.Nehezseg}"));
+        }
+
+        private void btnSzamlalo_Click(object sender, RoutedEventArgs e)
+        {
+            lblAllomanySzam.Content = string.Join("", lista.Count());
+        }
+
+        private void btnKereses_Click(object sender, RoutedEventArgs e)
+        {
+            /// <summary>
+            /// A datagridben írja ki a túrák leírását
+            /// </summary>
+            if (tbErtek.Text != null)
+            {
+                lblEredmeny.Content = $"{lista.Count(x => x.IdoTartam <= int.Parse(tbErtek.Text))} db túrára volt találat";
+
+                var lista2 = lista.Where(x => x.IdoTartam <= int.Parse(tbErtek.Text));
+
+                dgTablauzat.ItemsSource = "";
+                dgTablauzat.ItemsSource = lista2;
+                dgTablauzat.Items.Refresh();
+            }
+            else 
+            {
+                MessageBox.Show("Adjon meg egy értéket a fenti textboxba!");
+            }
+            
+        }
+
+        private void btnGomb_Click(object sender, RoutedEventArgs e)
+        {
+            lbTeljesitesiAtlag.Content = $"{Math.Round(lista.Where(x => x.Nev.Contains("Pilis")).Average(x => x.IdoTartam))} perc";
+        }
+
+        private void btnLeghosszabb_Click(object sender, RoutedEventArgs e)
+        {
+            var leghosszabbUt = lista.MaxBy(x => x.Tavolsag);
+            lblLegosszabb.Content = $"{leghosszabbUt.Nev};{leghosszabbUt.Tavolsag};{leghosszabbUt.IdoTartam}";
+        }
+    }
+}
---
+++ Szakasz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DolgozatWpf
+{
+    class Szakasz
+    {
+        String nev;
+        double tavolsag;
+        int tengerszintFeletti;
+        int idoTartam;
+        string nehezseg;
+
+
+        public Szakasz(string sor)
+        {
+            var mezok = sor.Split(';');
+            this.nev = mezok[0];
+            this.tavolsag = double.Parse(mezok[1]);
+            this.tengerszintFeletti = int.Parse(mezok[2]);
+            this.idoTartam = int.Parse(mezok[3]);
+        }
+
+        public string Nev { get => nev;}
+        public double Tavolsag { get => tavolsag;}
+        public int TengerszintFeletti { get => tengerszintFeletti; }
+        public int IdoTartam { get => idoTartam;}
+        public string Nehezseg { get => nehezseg; set => nehezseg = value; }
+
+        public static void NehezsegMegadas(List<Szakasz> lista)
+        {
+            foreach (var item in lista)
+            {
+                if (item.Tavolsag <= 5)
+                {
+                    item.nehezseg = "Könnyű";
+                }
+                else if (item.Tavolsag > 5 && item.Tavolsag < 10) 
+                {
+                    item.nehezseg = "Közepes";
+                }
+                else if (item.Tavolsag > 10)
+                {
+                    item.nehezseg = "Nehéz";
+                }
+            }
+        }
+    }
+}
